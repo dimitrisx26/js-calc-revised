0f5a86b87361b1e5330d04bb8457be0a50aad006
dimitrisx26 <dxynos@outlook.com>
2023-02-01 21:05:09 +0200

    Some more changes in the logic and added  features

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnNum = document.querySelectorAll(".num-key");
 const btnOp = document.querySelectorAll(".op-key");
 const btnPoint = document.querySelector(".point-key");
 const btnResult = document.querySelector(".result-key");
+const btnDel = document.querySelector(".del-key");
 
 let op = "";
 let num1 = "";
@@ -37,21 +38,19 @@ function operate() {
     } else if (op === '*') {
         return multiply(num1, num2);
     } else if (op === '/') {
-        if (num2 === 0) {
-            resultScreen.innerHTML = "Error";
-        } else{
-            return divide(num1, num2);
-        }
-
+        return divide(num1, num2);
     } else if (op === '%') {
         return modulo(num1, num2);
     }
 }
 
 function display(num) {
- 
-   
-    if (num1 == "") {
+   if (num == "." && num1.includes(".") && op == "") {
+        return;
+   } else if (num == "." && num1 === "") {
+        num1 = "0."
+        resultScreen.innerHTML = num1;
+   } else if (num1 == "") {
         num1 = num;
         resultScreen.innerHTML = num1;
     } else if (op == "") {
@@ -59,7 +58,12 @@ function display(num) {
         resultScreen.innerHTML = num1;
     } 
 
-    if (num1 != "" && num2 == "" && op != "") {
+    if (num == "." && num2.includes(".") && op != "") {
+        return;
+    } else if (num == "." && num2 == "" && op != "") {
+        num2 = "0."
+        resultScreen.innerHTML = num2;
+    } else if (num1 != "" && num2 == "" && op != "") {
         num2 = num;
         resultScreen.innerHTML = num2;
     } else if (op != "") {
@@ -68,6 +72,28 @@ function display(num) {
     }
 
 }
+
+btnDel.addEventListener("click", function () {
+    let tempNum;
+    if (op === "") {
+        tempNum = num1.slice(0, -1);
+        num1 = tempNum;
+        if (num1 == "") {
+            resultScreen.textContent = "0";
+        } else {
+            resultScreen.textContent = num1;
+        }
+        
+    } else {
+        tempNum = num2.slice(0, -1);
+        num2 = tempNum;
+        if (num2 == "") {
+            resultScreen.textContent = "0";
+        } else {
+            resultScreen.textContent = num2;
+        }
+    }
+})
 
 function clearScreen() {
     num1 = "";
@@ -79,7 +105,14 @@ function clearScreen() {
 
 function calcResult() {
     result = operate();
-    const temp = result;
+    let temp;
+    if ((result % 2) === 0) {
+        temp = result;
+    } else {
+        temp = result.toFixed(3);
+        temp = parseFloat(temp);
+    }
+    
     clearScreen();
     num1 = temp;
     result = temp;
