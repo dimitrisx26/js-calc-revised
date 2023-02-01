c1b4869e638e25e875b80cb5acd00d79bab25f90
dimitrisx26 <dxynos@outlook.com>
2023-02-01 17:08:57 +0200

    The main logic of the calc is made

--- script.js
+++ script.js
@@ -6,32 +6,31 @@ const btnPoint = document.querySelector(".point-key");
 const btnResult = document.querySelector(".result-key");
 
 let op = "";
-let num1 = null;
-let num2 = null;
-let result = null;
+let num1 = "";
+let num2 = "";
+let result = "";
 
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
 function modulo(num1, num2) {
-    return num1 % num2;
+    return Number(num1) % Number(num2);
 }
 
-function operate(op, num1, num2) {
-    Number(num1);
-    Number(num2);
+function operate() {
+    console.log(op, num1, num2);
     if (op === '+') {
         return add(num1, num2);
     } else if (op === '-') {
@@ -39,42 +38,80 @@ function operate(op, num1, num2) {
     } else if (op === '*') {
         return multiply(num1, num2);
     } else if (op === '/') {
-        return divide(num1, num2);
+        if (num2 === 0) {
+            resultScreen.innerHTML = "Error";
+        } else{
+            return divide(num1, num2);
+        }
+
     } else if (op === '%') {
         return modulo(num1, num2);
     }
 }
 
-btnNum.forEach(btn => {
-    btn.addEventListener("click", event => {
-        console.log(btn.innerHTML);
-        num1 = num1 + btn.innerHTML;
+function display(num) {
+ 
+   
+    if (num1 == "") {
+        num1 = num;
         resultScreen.innerHTML = num1;
+    } else if (op == "") {
+        num1 = num1 + num;
+        resultScreen.innerHTML = num1;
+    } 
+
+    if (num1 != "" && num2 == "" && op != "") {
+        num2 = num;
+        resultScreen.innerHTML = num2;
+    } else if (op != "") {
+        num2 = num2 + num;
+        resultScreen.innerHTML = num2;
+    }
+
+    if (result === num1) {
+        const temp = result;
+        clearScreen();
+        num1 = temp;
+        resultScreen.innerHTML = num1;
+    }
+    console.log("op ", op, "num ", num, "num1 ", num1, "num2 ", num2);
+
+}
+
+function clearScreen() {
+    num1 = "";
+    num2 = "";
+    op = "";
+    result = "";
+    console.log("clear: ", num1, num2, op, result);
+    resultScreen.innerHTML = '0';
+}
+
+function calcResult() {
+    result = operate();
+    num1 = result;
+    resultScreen.innerHTML = result;
+}
+
+btnNum.forEach(number => {
+    number.addEventListener("click", event => {
+        const num = number.innerHTML;
+        display(num);
     });
 });
 
-btnOp.forEach(btn => {
-    btn.addEventListener("click", event => {
-        console.log(btn.innerHTML);
-        if(num1 != null && op == "") {
-            op = btn.innerHTML;
+btnOp.forEach(operator => {
+    operator.addEventListener("click", event => {
+        if(num1 != "" && op == "") {
+            op = operator.innerHTML;
         }
     });
 });
 
 btnClear.addEventListener("click", event => {
-    console.log(btnClear.innerHTML);
-    console.log(btnResult.innerHTML);
-    num1 = null;
-    num2 = null;
-    op = '';
-    result = null;
-    resultScreen.innerHTML = '0';
+    clearScreen();
 });
 
 btnResult.addEventListener("click", event => {
-    result = operate(op, num1, num2);
-    num1 = result;
-    resultScreen.innerHTML = result;
+    calcResult();
 });
-
